07aff57582836d4b93f9812f21d912f1166396dd
Tatag Winaryo <aku4layy@gmail.com>
2020-04-04 19:31:57 +0700

    add datatable

--- application/controllers/registered/PesertaController.php
+++ application/controllers/registered/PesertaController.php
@@ -27,8 +27,10 @@ class PesertaController extends CI_Controller
 		if ($this->mysession->loginGetData('role')=='fdd38312da2d5ddc4b90a49aaa2bcf52d586572db5ce37cb2630799476aa13e4') {
 
 			$data = $this->globalfunction->getBasicData();
-
 			$data['onpage'] = 'PESERTA';
+
+			$userCond = array('role_id' => '83bbe0cd25d8cc4b8c076497a57d4b6452e84946b9042dc7983a7806a1f636cf');
+			$data['peserta'] = $this->BasicQuery->selectAllResult('user',$userCond);
 			// echo(json_encode($data));
 			$this->load->view('registered/peserta',$data);
 
--- application/views/registered/peserta.php
+++ application/views/registered/peserta.php
@@ -6,9 +6,17 @@
       $this->load->helper(array('form', 'url'));
 
       $this->load->view('registered/layouts/header');
+    ?>
+    
+    <link href="<?php echo base_url();?>assets/admin/assets/plugins/bootstrap-select2/select2.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="<?php echo base_url();?>assets/admin/assets/plugins/jquery-datatable/css/jquery.dataTables.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url();?>assets/admin/assets/plugins/datatables-responsive/css/datatables.responsive.css" rel="stylesheet" type="text/css" media="screen" />
 
+    <?php   
       $this->load->view('registered/layouts/csshandler');
     ?>
+
+    
 
   </head>
 
@@ -25,14 +33,59 @@
 
         <!-- BEGIN PAGE CONTAINER-->
         <div class="page-content">
+
           <div class="content">
-            <div class="page-title">
 
 
-              <h3>On Progress </h3>
+              <div class="row-fluid">
+                <div class="span12">
+                  <div class="grid simple ">
+                    <div class="grid-title">
+                      <h4>DAFTAR SEMUA PESERTA</h4>
+                      <div class="tools">
+                        <a href="javascript:;" class="collapse"></a>
+                        <!-- <a href="#grid-config" data-toggle="modal" class="config"></a> -->
+                        <!-- <a href="javascript:;" class="reload"></a> -->
+                        <!-- <a href="javascript:;" class="remove"></a> -->
+                      </div>
+                    </div>
+                    <div class="grid-body ">
+                      <table class="table table-striped" id="example2">
+                        <thead>
+                          <tr>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>Phone Number</th>
+                            <th>Country</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                            foreach ($peserta as $key => $value) {
+                          ?>    
+                              <tr class="">
+                                <td><?php echo($value['id']) ?></td>
+                                <td><?php echo($value['fullname']) ?></td>
+                                <td><?php echo($value['phone_number']) ?></td>
+                                <td class="center"><?php echo($value['country']) ?></td>
+                                <td class="center">Belum tersedia</td>
+                              </tr>
+
+                          <?php  }
+                          ?>
 
 
-            </div>
+                          
+                          
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
           </div>
         </div>
         <!-- END PAGE CONTAINER -->
@@ -42,6 +95,17 @@
 
     <!-- jshandler -->
     <?php $this->load->view('registered/layouts/jshandler');  ?>
+
+     <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <script src="<?php echo base_url();?>assets/admin/assets/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url();?>assets/admin/assets/plugins/jquery-datatable/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url();?>assets/admin/assets/plugins/jquery-datatable/extra/js/dataTables.tableTools.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/admin/assets/plugins/datatables-responsive/js/datatables.responsive.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/admin/assets/plugins/datatables-responsive/js/lodash.min.js"></script>
+    <!-- END PAGE LEVEL JS INIT -->
+    <script src="<?php echo base_url();?>assets/admin/assets/js/datatables.js" type="text/javascript"></script>
+    <!-- END JAVASCRIPTS -->
+
 
   </body>
 </html>
