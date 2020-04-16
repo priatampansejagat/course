d561a4790a2f77a0a26681f12e6827a2a86b74ff
MZ. Naufal Maulana <mznaufalmaulana@gmail.com>
2020-04-16 22:23:01 +0700

    pagination

--- application/views/visitor/course/list.php
+++ application/views/visitor/course/list.php
@@ -25,13 +25,6 @@
         <nav aria-label="Page navigation">
             <!-- pagination -->
             <ul class="pagination">
-                <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">2</a></li>
-                <li>
-                    <a href="#" aria-label="Next">
-                        <span aria-hidden="true">›</span>
-                    </a>
-                </li>
             </ul>
         </nav>
     </section>
@@ -51,31 +44,42 @@
                 url: get_datatable_url
             },
             success: function(data) {
+                var pageSize = 3;
                 var course = $('.course-list');
                 var dataJson = JSON.parse(data);
-                if (dataJson.status == 200) {
-                    $.each(dataJson.data, function(key, value) {
-                        var date_custom = new Date(Date.parse(value.start_date));
-                        course.append('<li>' +
-                            '<div class="alignright">' +
-                            '<a href="coursedetail/' + value.id + '" class="btn btn-warning text-uppercase">detail</a>' +
-                            '</div>' +
-                            '<div class="alignleft">' +
-                            '<time datetime="2011-01-12" class="time text-uppercase">' +
-                            '<strong class="date fw-normal">' + (date_custom.getDate() < 10 ? '0' + date_custom.getDate() : date_custom.getDate()) + '</strong>' +
-                            '<strong class="month fw-light font-lato">' + month(date_custom.getMonth()) + '</strong>' +
-                            '<strong class="day fw-light font-lato">' + (date_custom.getFullYear()) + '</strong>' +
-                            '</time>' +
-                            '</div>' +
-                            '<div class="description-wrap">' +
-                            '<h3 class="list-heading">' + value.title + '</h3>' +
-                            '<address> by : ' + value.mentor.fullname + '</address>' +
-                            '</div>' +
-                            '</li>');
-                    });
-                } else {
-                    area.append('<option>Data tidak tersedia</option>');
+                var pageCount = dataJson.data.length / pageSize;
+
+                for (var i = 0; i < pageCount; i++) {
+                    $(".pagination").append('<li><a href="#">' + (i + 1) + '</a></li> ');
                 }
+                $(".pagination li").first().addClass("active");
+
+                showPage = function(page) {
+                    $(".line-content").hide();
+                    $(".line-content").each(function(n) {
+                        if (n >= pageSize * (page - 1) && n < pageSize * page)
+                            $(this).show();
+                    });
+                }
+                $.each(dataJson.data, function(key, value) {
+                    var date_custom = new Date(Date.parse(value.start_date));
+                    course.append('<li>' +
+                        '<div class="alignright">' +
+                        '<a href="coursedetail/' + value.id + '" class="btn btn-warning text-uppercase">detail</a>' +
+                        '</div>' +
+                        '<div class="alignleft">' +
+                        '<time datetime="2011-01-12" class="time text-uppercase">' +
+                        '<strong class="date fw-normal">' + (date_custom.getDate() < 10 ? '0' + date_custom.getDate() : date_custom.getDate()) + '</strong>' +
+                        '<strong class="month fw-light font-lato">' + month(date_custom.getMonth()) + '</strong>' +
+                        '<strong class="day fw-light font-lato">' + (date_custom.getFullYear()) + '</strong>' +
+                        '</time>' +
+                        '</div>' +
+                        '<div class="description-wrap">' +
+                        '<h3 class="list-heading">' + value.title + '</h3>' +
+                        '<address> by : ' + value.mentor.fullname + '</address>' +
+                        '</div>' +
+                        '</li>');
+                });
             }
         });
     });
